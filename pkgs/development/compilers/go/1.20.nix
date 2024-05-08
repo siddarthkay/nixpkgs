e622bebd1ca8af85398af96179286da83010b2e1
Siddarth Kumar <siddarthkay@gmail.com>
2024-05-08 19:24:17 +0530

    Update 1.20.nix

--- pkgs/development/compilers/go/1.20.nix
+++ pkgs/development/compilers/go/1.20.nix
@@ -50,7 +50,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://go.dev/dl/go${finalAttrs.version}.src.tar.gz";
-    hash = "sha256-xb+TR1HTHDFcHQu1+wIpZUX6bQiSNWb3pa/sgfLtJ9Y=";
+    hash = "sha256-xb+TR1HTHDFcHQu1+wIpZUX6bQiSNWb4pa/sgfLtJ9Y=";
   };
 
   strictDeps = true;
